135acdcb15b5bf0bfcd52955e0e0901ee3f0f825
Gabriela Melnik Carvalho <gabrielamcarvalho@gmail.com>
2020-09-29 15:05:12 +0100

    :hammer: changed website

--- js/about.js
+++
@@ -1,8 +0,0 @@
-
-  document.querySelector('#about__btn').addEventListener('click', () => {
-    document.querySelector('.container').classList.add('add-team');
-  });
-  //go to top of the page action
-  document.querySelector('#arrow-up').addEventListener('click', () => {
-    document.querySelector('.container').classList.remove('add-team');
-  });
--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-document.querySelector('.hamburger-menu').addEventListener('click', () => {
-  document.querySelector('.container').classList.toggle('change');
+// Get the modal
+var modal = document.getElementById('modal');
+const hamburger = document.getElementById('hamburger');
+const mainNav = document.getElementById('js-menu');
+
+// Get the button that opens the modal
+var btn = document.querySelector('.special-btn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+//hamburger
+hamburger.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+  hamburger.classList.toggle('rotate');
 });
-/*Reviews*/
